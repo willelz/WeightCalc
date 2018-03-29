409b02342d7579fd3623c22517812ca91575e80b
willelz <37827548+willelz@users.noreply.github.com>
2018-03-29 23:20:43 +0900

    ファイル分け

--- src/app.ts
+++ src/app.ts
@@ -71,39 +71,3 @@ function calc() {
     const ansel = <HTMLInputElement>document.getElementById('answer');
     ansel.innerText = String(ans);
 }
-
-//タブ切り替え
-(function () {
-    const menu = document.getElementById('menu');
-    const content = document.getElementById('tab_content');
-    if (!menu) {
-        return;
-    }
-    let menus = menu.getElementsByTagName('a');
-    let current: { page: HTMLElement, menu: HTMLElement };
-
-    for (let i = 0, l = menus.length; i < l; i++) {
-        tab_init(menus[i], i);
-    }
-    function tab_init(link: HTMLAnchorElement, index: number) {
-        const id = link.hash.slice(1);
-        const page = document.getElementById(id);
-        if (!page) return;
-        if (!current) { // 状態の初期化
-            current = { page: page, menu: link };
-            page.style.display = 'block';
-            link.className = 'active';
-        } else {
-            page.style.display = 'none';
-        }
-        link.onclick = function () {
-            current.page.style.display = 'none';
-            current.menu.className = '';
-            page.style.display = 'block';
-            link.className = 'active';
-            current.page = page;
-            current.menu = link;
-            return false;
-        };
-    }
-})();
