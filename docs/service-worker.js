16cdd74c0264f59edb9382350ea612a964fd8ec4
willelz <37827548+willelz@users.noreply.github.com>
2018-11-22 00:40:23 +0900

    update workbox

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,3 +1,11 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
 workbox.precaching.precacheAndRoute([
   {
     "url": "angle_horizontal.4f490c42.svg",
